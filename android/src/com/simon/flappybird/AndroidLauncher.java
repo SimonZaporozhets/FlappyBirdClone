47f99d0b940c71e0fa067222ffdedf6abeef19c7
SimonZaporozhets <56390222+SimonZaporozhets@users.noreply.github.com>
2021-05-04 16:28:32 +0200

    Initial commit

---
+++ android/src/com/simon/flappybird/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.simon.flappybird;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.simon.flappybird.FlappyBird;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FlappyBird(), config);
+	}
+}
